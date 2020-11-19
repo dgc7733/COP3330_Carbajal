5e9aab39222267bd7c72463581752d2b5b51a921
dgc7733 <70238179+dgc7733@users.noreply.github.com>
2020-11-18 23:23:25 -0500

    Update App.java

--- Carbajal_p4/src/App.java
+++ Carbajal_p4/src/App.java
@@ -6,7 +6,7 @@ public class App {
          displayMainMenu();
          mainMenuTaskChoice();
          ListOperationMenu();
-         ListOperatioMenuChoice();
+         ListOperationMenuChoice();
 
         }
 
